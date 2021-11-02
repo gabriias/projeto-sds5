9d63d66029515393dc9db341f2923fcc6f5796fc
Nelkisa Matias <gabrielamatias60@gmail.com>
2021-11-02 18:14:08 -0300

    Criando os componentes estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
+
 function App() {
   return (
-    <div >
-      <h1>opa</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1>opa</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
